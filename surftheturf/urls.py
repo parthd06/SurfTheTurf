be498418adb01b9874725620b4a594ce2bd6ceeb
Parth <daliparth06@gmail.com>
2021-12-04 11:49:19 +0530

    changes

--- surftheturf/urls.py
+++ surftheturf/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls.conf import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('turf.urls')),
