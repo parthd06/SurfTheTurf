ae3d0864845645b803ac90950fbb7c80dffc2d6a
Parth <daliparth06@gmail.com>
2021-12-04 12:52:15 +0530

    new changes

--- turf/urls.py
+++ turf/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from . import views
-# from django_email_verification import urls as email_urls
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -12,23 +11,9 @@ urlpatterns = [
     path("logout", views.logout, name="logout"),
     path("contactus", views.contactus, name="contactus"),
     path("aboutus", views.aboutus, name="aboutus"),
-    # path('about', views.about, name="about"),
-    # path('services', views.services, name="services"),
-    # path('contact', views.contact, name="contact"),
-    # path('destination_details/<int:id>', views.destination_details, name='destination_details'),
-    # path('booking/<int:id>', views.booking, name="booking"),
-    # path('receipt', views.receipt, name='receipt'),
-    # path('search', views.search, name='search'),
-    # path('turfDateSelection', views.turfDateSelection, name="turfDateSelection"),
-    # path('turfBookings', views.turfBookings, name="turf_bookings"),
     path('turfBilling', views.turfBilling, name='turfBilling'),
-    # path('confirm_booking', views.confirm_booking, name='confirm_booking'),
     path('orderHistory', views.orderHistory, name="orderHistory"),
     path('allBookings', views.allBookings, name="allBookings"),
-    # path('searchBooking', views.searchBooking, name="searchBooking"),
-    # path('Booked', views.Booked, name="Booked"),
-    # path('email/', include(email_urls)),
     path('delete_booking/<int:id>', views.delete_booking, name="delete_booking"),
-     path('success', views.success, name='success')
-    # path('sortBy', views.sortBy, name="sortBy"),
+    path('success', views.success, name='success')
 ]
--- turf/views.py
+++ turf/views.py
@@ -82,7 +82,7 @@ def signup(request):
         else:
             user = User.objects.create_user(username=username, password=password, email=email)
             user.save()
-            return redirect('book_now')
+            return redirect('login')
     else:
         return render(request, 'signUp.html')
 
@@ -97,167 +97,157 @@ def contactus(request):
 def aboutus(request):
     return render(request, 'aboutus.html')
     
-# def services(request):
+# def booking(request, id):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         lastName = request.POST['lastName']
+#         fromCity = request.POST['fromCity']
+#         toCity = request.POST['toCity']
+#         depatureDate = request.POST['depatureDate']
+#         days = request.POST['days']
+#         noOfRooms = int(request.POST['noOfRooms'])
+#         noOfAdults = int(request.POST['noOfAdults'])
+#         noOfChildren = int(request.POST['noOfChildren'])
+#         email = request.POST['email']
+#         phoneNo = request.POST['phoneNo']
+#         totalAmount = int(request.POST['totalAmount'])
 
-#     dests = Destination.objects.all()
-#     return render(request, 'destinations.html', {'dests': dests})
+#         request.session['fname'] = username
+#         request.session['lname'] = lastName
+#         request.session['to_city'] = toCity
+#         request.session['from_city'] = fromCity
+#         request.session['depature_date'] = depatureDate
+#         request.session['arrival_date'] = days
+#         request.session['no_of_rooms'] = noOfRooms
+#         request.session['no_of_adults'] = noOfAdults
+#         request.session['no_of_children'] = noOfChildren
+#         request.session['email'] = email
+#         request.session['phone_no'] = phoneNo
+#         request.session['total_amount'] = totalAmount
 
-def booking(request, id):
+#         requiredRooms = 1
+#         if noOfAdults/3 > 1:
+#             requiredRooms = math.ceil(noOfAdults/3)
 
-    if request.method == 'POST':
-        username = request.POST['username']
-        lastName = request.POST['lastName']
-        fromCity = request.POST['fromCity']
-        toCity = request.POST['toCity']
-        depatureDate = request.POST['depatureDate']
-        days = request.POST['days']
-        noOfRooms = int(request.POST['noOfRooms'])
-        noOfAdults = int(request.POST['noOfAdults'])
-        noOfChildren = int(request.POST['noOfChildren'])
-        email = request.POST['email']
-        phoneNo = request.POST['phoneNo']
-        totalAmount = int(request.POST['totalAmount'])
+#         if noOfRooms < requiredRooms:
+#             noOfRooms = requiredRooms - noOfRooms
+#             messages.info(
+#                 request, 'For adding more travellers, Please add' + str(noOfRooms) + ' more rooms')
+#             return redirect('booking', id)
 
-        request.session['fname'] = username
-        request.session['lname'] = lastName
-        request.session['to_city'] = toCity
-        request.session['from_city'] = fromCity
-        request.session['depature_date'] = depatureDate
-        request.session['arrival_date'] = days
-        request.session['no_of_rooms'] = noOfRooms
-        request.session['no_of_adults'] = noOfAdults
-        request.session['no_of_children'] = noOfChildren
-        request.session['email'] = email
-        request.session['phone_no'] = phoneNo
-        request.session['total_amount'] = totalAmount
+#         if noOfRooms > noOfAdults:
+#             messages.info(request, 'Minimum 1 Adult is required per Room')
+#             return redirect('booking', id)
 
-        requiredRooms = 1
-        if noOfAdults/3 > 1:
-            requiredRooms = math.ceil(noOfAdults/3)
+#         if (noOfAdults + noOfChildren)/4 > 1:
+#             requiredRooms = math.ceil((noOfAdults + noOfChildren)/4)
 
-        if noOfRooms < requiredRooms:
-            noOfRooms = requiredRooms - noOfRooms
-            messages.info(
-                request, 'For adding more travellers, Please add' + str(noOfRooms) + ' more rooms')
-            return redirect('booking', id)
+#         if noOfRooms < requiredRooms:
+#             noOfRooms = requiredRooms - noOfRooms
+#             messages.info(
+#                 request, 'For adding more travellers, Please add' + str(noOfRooms) + 'more rooms')
+#             return redirect('booking', id)
 
-        if noOfRooms > noOfAdults:
-            messages.info(request, 'Minimum 1 Adult is required per Room')
-            return redirect('booking', id)
-
-        if (noOfAdults + noOfChildren)/4 > 1:
-            requiredRooms = math.ceil((noOfAdults + noOfChildren)/4)
-
-        if noOfRooms < requiredRooms:
-            noOfRooms = requiredRooms - noOfRooms
-            messages.info(
-                request, 'For adding more travellers, Please add' + str(noOfRooms) + 'more rooms')
-            return redirect('booking', id)
-
-        noOfRooms = requiredRooms
-        request.session['no_of_rooms'] = noOfRooms
-        print("No of rooms = ", noOfRooms)
-        print("Working")
-        # book = Booking(username=username, lastName=lastName, fromCity=fromCity, toCity=toCity, depatureDate=depatureDate, arrivalDate=arrivalDate, noOfRooms=noOfRooms, noOfAdults=noOfAdults, noOfChildren=noOfChildren, email=email,phoneNo=phoneNo, totalAmount=totalAmount)
-
-        # book.save()
-        return redirect('receipt')
-    else:
-        return render(request, 'booking.html')
+#         noOfRooms = requiredRooms
+#         request.session['no_of_rooms'] = noOfRooms
+#         print("No of rooms = ", noOfRooms)
+#         print("Working")
+#         return redirect('receipt')
+#     else:
+#         return render(request, 'booking.html')
 
 
-@login_required(login_url='/accounts/login')
-def receipt(request):
-    first_name = request.session.get('fname')
-    print(first_name)
-    last_name = request.session.get('lname')
-    print(last_name)
+# @login_required(login_url='/accounts/login')
+# def receipt(request):
+    # first_name = request.session.get('fname')
+    # print(first_name)
+    # last_name = request.session.get('lname')
+    # print(last_name)
 
-    tour_amount = int(request.session.get('total_amount'))  # Per person
-    print(tour_amount)
-    adults = int(request.session.get('no_of_adults'))
-    print(adults)
-    rooms = int(request.session.get('no_of_rooms'))
-    print(rooms)
-    children = int(request.session.get('no_of_children'))
-    print(adults)
-    if rooms > 1:
-        totalCost = tour_amount*adults + tour_amount*children/2 + rooms*tour_amount/4
-    else:
+    # tour_amount = int(request.session.get('total_amount'))  # Per person
+    # print(tour_amount)
+    # adults = int(request.session.get('no_of_adults'))
+    # print(adults)
+    # rooms = int(request.session.get('no_of_rooms'))
+    # print(rooms)
+    # children = int(request.session.get('no_of_children'))
+    # print(adults)
+    # if rooms > 1:
+    #     totalCost = tour_amount*adults + tour_amount*children/2 + rooms*tour_amount/4
+    # else:
 
-        totalCost = tour_amount*adults + tour_amount*children/2
+    #     totalCost = tour_amount*adults + tour_amount*children/2
 
-    request.session['total_amount'] = str(totalCost)
-    print("Hello")
+    # request.session['total_amount'] = str(totalCost)
+    # print("Hello")
 
-    print(totalCost)
-    request.session['total_amount'] = tour_amount
+    # print(totalCost)
+    # request.session['total_amount'] = tour_amount
 
-    today = date.today()
+    # today = date.today()
 
-    t = time.localtime()
-    currentTime = time.strftime("%H:%M:%S", t)
-    return render(request, 'receipt.html', {'totalCost': totalCost, 'date': today, 'currentTime': currentTime})
+    # t = time.localtime()
+    # currentTime = time.strftime("%H:%M:%S", t)
+    # return render(request, 'receipt.html', {'totalCost': totalCost, 'date': today, 'currentTime': currentTime})
 
 
-def search(request):
+# def search(request):
 
-    # dests = Destination.objects.all()
-    query = request.GET['query']
-    # budget = request.GET['budget']
-    price = Destination.objects.all()
-    # print(price.price)
-    print(query)
-    # print("Price = ", budget)
-    dests = Destination.objects.filter(name__icontains=query)
-    print(dests)
-    # dests = Destination.objects.filter(price__lt = budget)
+    # # dests = Destination.objects.all()
+    # query = request.GET['query']
+    # # budget = request.GET['budget']
+    # price = Destination.objects.all()
+    # # print(price.price)
+    # print(query)
+    # # print("Price = ", budget)
+    # dests = Destination.objects.filter(name__icontains=query)
     # print(dests)
+    # # print(dests)
 
-    return render(request, 'search.html', {'dests': dests, 'query': query})
-    # return HttpResponse('This is search')
+    # return render(request, 'search.html', {'dests': dests, 'query': query})
+    # # return HttpResponse('This is search')
 
 
-def confirm_booking(request):
-    if request.method == 'POST':
-        fullName = request.POST['fullName']
-        fromCity = request.POST['fromCity']
-        toCity = request.POST['toCity']
-        depatureDate = request.POST['depatureDate']
-        arrivalDate = request.POST['days']
-        noOfRooms = int(request.POST['noOfRooms'])
-        noOfAdults = int(request.POST['noOfAdults'])
-        noOfChildren = int(request.POST['noOfChildren'])
-        email = request.POST['email']
-        phoneNo = request.POST['phoneNo']
-        amountPerPerson = request.POST['amountPerPerson']
-        totalAmount = float(request.POST['totalAmount'])
-        userName = request.user.username
+# def confirm_booking(request):
+    # if request.method == 'POST':
+    #     fullName = request.POST['fullName']
+    #     fromCity = request.POST['fromCity']
+    #     toCity = request.POST['toCity']
+    #     depatureDate = request.POST['depatureDate']
+    #     arrivalDate = request.POST['days']
+    #     noOfRooms = int(request.POST['noOfRooms'])
+    #     noOfAdults = int(request.POST['noOfAdults'])
+    #     noOfChildren = int(request.POST['noOfChildren'])
+    #     email = request.POST['email']
+    #     phoneNo = request.POST['phoneNo']
+    #     amountPerPerson = request.POST['amountPerPerson']
+    #     totalAmount = float(request.POST['totalAmount'])
+    #     userName = request.user.username
 
-        books = ConfirmBooking(fullName=fullName, fromCity=fromCity, toCity=toCity,
-                               depatureDate=depatureDate, days=arrivalDate, noOfRooms=noOfRooms, noOfAdults=noOfAdults,
-                               noOfChildren=noOfChildren, email=email, phoneNo=phoneNo, amountPerPerson=amountPerPerson,
-                               totalAmount=totalAmount, userName=userName)
-        books.save()
+    #     books = ConfirmBooking(fullName=fullName, fromCity=fromCity, toCity=toCity,
+    #                            depatureDate=depatureDate, days=arrivalDate, noOfRooms=noOfRooms, noOfAdults=noOfAdults,
+    #                            noOfChildren=noOfChildren, email=email, phoneNo=phoneNo, amountPerPerson=amountPerPerson,
+    #                            totalAmount=totalAmount, userName=userName)
+    #     books.save()
 
-        message = render_to_string('order_placed_body.html', {'fullName': fullName, 'fromCity': fromCity, 'toCity': toCity, 'depatureDate': depatureDate, 'arrivalDate': arrivalDate,
-                                   'noOfRooms': noOfRooms, 'noOfAdults': noOfAdults, 'noOfChildren': noOfChildren, 'email': email, 'phoneNo': phoneNo, 'amountPerPerson': amountPerPerson, 'totalAmount': totalAmount})
-        msg = EmailMessage(
-            'Tripology',
-            message,
-            settings.EMAIL_HOST_USER,
-            [request.user.email]
-        )
-        msg.content_subtype = "html"  # Main content is now text/html
-        msg.send()
+    #     message = render_to_string('order_placed_body.html', {'fullName': fullName, 'fromCity': fromCity, 'toCity': toCity, 'depatureDate': depatureDate, 'arrivalDate': arrivalDate,
+    #                                'noOfRooms': noOfRooms, 'noOfAdults': noOfAdults, 'noOfChildren': noOfChildren, 'email': email, 'phoneNo': phoneNo, 'amountPerPerson': amountPerPerson, 'totalAmount': totalAmount})
+    #     msg = EmailMessage(
+    #         'Tripology',
+    #         message,
+    #         settings.EMAIL_HOST_USER,
+    #         [request.user.email]
+    #     )
+    #     msg.content_subtype = "html"  # Main content is now text/html
+    #     msg.send()
 
-        print("Mail successfully sent")
+    #     print("Mail successfully sent")
 
-        print("User Added")
+    #     print("User Added")
 
-        return redirect('/')
-    else:
-        return render(request, 'booking.html')
+    #     return redirect('/')
+    # else:
+    #     return render(request, 'booking.html')
 
 
 update = {"1"}
@@ -860,7 +850,7 @@ def searchBooking(request):
     return render(request, 'allBookings.html', {'bookings': bookings, 'query': query})
     # return HttpResponse('This is search')
 
-#
+
 #
 # present = datetime.now()
 # dayTobeDeleated = (datetime.now() - timedelta(days=1)).strftime("%A")
